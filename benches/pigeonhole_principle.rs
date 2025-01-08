82624db0407a7ec6386018b785cc310668d07768
dabzr <dylanab@pm.me>
2025-01-08 15:48:52 -0300

    atualizando problema da casa dos pombos para ser criado na CNF

--- benches/pigeonhole_principle.rs
+++ benches/pigeonhole_principle.rs
@@ -3,33 +3,32 @@ use saturno::types::*;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn create_php_formula(n: usize) -> Formula {
-    let mut gamma = atom("neutral1");
+    let mut gamma = atom("neutral1"); 
     let mut delta = atom("neutral2"); 
-
     for i in 1..=n + 1 {
         let clause = (1..=n)
             .map(|j| atom(&format!("p{}_{}", i, j)))
-            .reduce(or)
-            .unwrap(); 
+            .reduce(|acc, lit| or(acc, lit))
+            .unwrap();
         gamma = and(gamma, clause);
     }
-
-    for i in 1..=n + 1 { 
-        for k in (i + 1)..=n + 1 {
-            for j in 1..=n { 
-                let clause = and(
-                    atom(&format!("p{}_{}", i, j)),
-                    atom(&format!("p{}_{}", k, j)),
+    for j in 1..=n {
+        for i in 1..=n + 1 {
+            for k in i + 1..=n + 1 {
+                let clause = or(
+                    not(atom(&format!("p{}_{}", i, j))),
+                    not(atom(&format!("p{}_{}", k, j))),
                 );
-                delta = or(delta, clause);
+                delta = and(delta, clause);
             }
         }
     }
     implies(gamma, delta)
 }
 
+
 fn bench_dpll(c: &mut Criterion) {
-    let formula = create_php_formula(2);
+    let formula = create_php_formula(3);
     c.bench_function("dpll", |b| {
         b.iter(|| {
             formula.sat_dpll();
--- examples/pigeonhole.rs
+++ examples/pigeonhole.rs
@@ -1,34 +1,34 @@
-use saturno::types::Formula;
 use saturno::types::*;
 
 fn create_php_formula(n: usize) -> Formula {
     let mut gamma = atom("neutral1");
-    let mut delta = atom("neutral2"); 
-
+    let mut delta = atom("neutral2");
     for i in 1..=n + 1 {
         let clause = (1..=n)
-            .map(|j| atom(&format!("p{}_{}", i, j)))
-            .reduce(or)
-            .unwrap(); 
+            .map(|j| atom(&format!("p{}_{}", i, j))) 
+            .reduce(|acc, lit| or(acc, lit)) 
+            .unwrap();
         gamma = and(gamma, clause);
     }
 
-    for i in 1..=n + 1 { 
-        for k in (i + 1)..=n + 1 {
-            for j in 1..=n { 
-                let clause = and(
-                    atom(&format!("p{}_{}", i, j)),
-                    atom(&format!("p{}_{}", k, j)),
+    for j in 1..=n {
+        for i in 1..=n + 1 {
+            for k in i + 1..=n + 1 {
+                let clause = or(
+                    not(atom(&format!("p{}_{}", i, j))),
+                    not(atom(&format!("p{}_{}", k, j))),
                 );
-                delta = or(delta, clause);
+                delta = and(delta, clause); 
             }
         }
     }
+
     implies(gamma, delta)
 }
 
 fn main() {
-    let f = create_php_formula(2);
-    let f_sat = f.sat_dpll();
-    println!("Is satisfiable? {}", f_sat);
+    let n = 3; 
+    let formula = create_php_formula(n);
+    let f_sat = formula.sat_dpll();
+    println!("A fórmula é satisfatível? {}", f_sat);
 }
